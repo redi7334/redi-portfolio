061257a1eec86579e7a3e5b909352fa8320d13ca
unknown <redimema2211@gmail.com>
2026-02-27 23:59:30 +0100

    Portfolio initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Message sent! (We will connect this to real email later)");
+  form.reset();
+});
